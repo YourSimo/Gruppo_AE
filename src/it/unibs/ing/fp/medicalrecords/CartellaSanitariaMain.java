a6ae24d0f7e453bee19708b9af3f312d8e98d925
Fedeclimber96 <federico.juve59@gmail.com>
2016-06-16 16:15:54 +0200

    modifica alle classi

--- src/it/unibs/ing/fp/medicalrecords/CartellaSanitariaMain.java
+++ src/it/unibs/ing/fp/medicalrecords/CartellaSanitariaMain.java
@@ -1,6 +1,9 @@
 package it.unibs.ing.fp.medicalrecords;
 
+
 import java.io.File;
+
+import it.unibs.ing.fp.library.InputData;
 import it.unibs.ing.fp.library.OutputData;
 
 public class CartellaSanitariaMain {
@@ -14,6 +17,20 @@ public class CartellaSanitariaMain {
 	
 	private static final String MSG_SALVA = "SALVATAGGIO DATI";
 	
+	private final static String MSG_INTRO_INSERIMENTO = "Benvenuto. Di seguito potr‡ inserire i dati personali per la sua cartella medica; tutti i campi sono obbligatori";
+	private final static String MSG_NOME = "Inserisca il proprio nome:";
+	private final static String MSG_COGNOME = "Inserisca il proprio cognome:";
+	private final static String MSG_INDIRIZZO = "Inserisca il proprio indirizzo:";
+	private final static String MSG_TELEFONO = "Inserisca il proprio numero di telefono";
+	private final static String MSG_EMAIL = "Inserisca la propria email:";
+	private final static String MSG_DATA_NASCITA = "Inserisca la propria data di nascita:";
+	private final static String MSG_LUOGO_NASCITA = "Inserisca il proprio luogo di nascita:";
+	private final static String MSG_GENERE =  "Inserisca il proprio genere:";
+	private final static String MSG_CODICE_FISCALE = "Inserisca il proprio codice fiscale";
+	private final static String MSG_GRUPPO_SANGUIGNO = "Inserisca il proprio gruppo sanguigno";
+    
+	private final static String MSG_ERRORE_INSERIMENTO = "Errore nell'inerimento dati. Dato non valido. Ritenti."
+
 	public static void main(String[] args) {
 		printMsg(MSG_INTRO);
 		
@@ -71,13 +88,94 @@ public class CartellaSanitariaMain {
 	private static CartellaSanitaria makeMedicalRecords() {
 		return new CartellaSanitaria(makePatient(), makeExamList());
 	}
-	
+//////////////////////////////////////////////////////////////////	
 	private static Paziente makePatient() {
+		
+
+		System.out.println(MSG_INTRO_INSERIMENTO);
+		//************************************************
+		String nome = InputData.readString(MSG_NOME);
+		
+		while (validit‡Nome(nome) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    nome = null;
+			nome = InputData.readString(MSG_NOME);	
+		};
+		//**************************************************
+		String cognome = InputData.readString(MSG_COGNOME);
+		
+		while (validit‡Cognome(cognome) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    cognome = null;
+			cognome = InputData.readString(MSG_COGNOME);	
+		};
+		//**************************************************
+		String indirizzo = InputData.readString(MSG_INDIRIZZO);
+		
+		
+		
+		
+		
+		
+		
+		
+		String telefono = InputData.readString (MSG_TELEFONO);
+		String email = InputData.readString (MSG_EMAIL);
+		String dataNascita = InputData.readString (MSG_DATA_NASCITA);
+		String luogoNascita = InputData.readString (MSG_LUOGO_NASCITA);
+		String genere = InputData.readString (MSG_GENERE);
+		String codiceFiscale = InputData.readString (MSG_CODICE_FISCALE);
+		//codice sanitario
+		
+		//controllo validit‡ dati
+		String gruppoSanguigno = InputDati.leggiStringa (MSG_GRUPPO_SANGUIGNO);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		return new Paziente();
 	}
 	
 	private static ListaEsami makeExamList() {
 		return new ListaEsami();
 	}
+	
+//////////////////////////////////////////////////////////////////////	 
+    public static boolean validit‡Nome (String nome){
+    	for(int i = 0; i < nome.length(); i++)
+    	{
+    		  char carattere = nome.charAt(i);
+    		  if(carattere<65 || ((carattere>90) && (carattere<97) || carattere>122))return false;
+    	
+    	}
+    	return true;
+    	}
+/////////////////////////////////////////////////////////////////////////	
+	public static boolean validit‡Cognome (String cognome){
+		return validit‡Nome(cognome);
+	}
+//////////////////////////////////////////////////////////////////////	
+	
+	
+	
+	
+	
+	
 }
 
--- src/it/unibs/ing/fp/medicalrecords/Paziente.java
+++ src/it/unibs/ing/fp/medicalrecords/Paziente.java
@@ -15,18 +15,7 @@ public class Paziente {
 	private String gruppoSanguigno;
 	
 	
-	private final static String MSG_INTRO_INSERIMENTO = "Benvenuto. Di seguito potrà inserire i dati personali per la sua cartella medica; tutti i campi sono obbligatori";
-	private final static String MSG_NOME = "Inserisca il proprio nome:";
-	private final static String MSG_COGNOME = "Inserisca il proprio cognome:";
-	private final static String MSG_INDIRIZZO = "Inserisca il proprio indirizzo:";
-	private final static String MSG_TELEFONO = "Inserisca il proprio numero di telefono";
-	private final static String MSG_EMAIL = "Inserisca la propria email:";
-	private final static String MSG_DATA_NASCITA = "Inserisca la propria data di nascita:";
-	private final static String MSG_LUOGO_NASCITA = "Inserisca il proprio luogo di nascita:";
-	private final static String MSG_GENERE =  "Inserisca il proprio genere:";
-	private final static String MSG_CODICE_FISCALE = "Inserisca il proprio codice fiscale";
-	private final static String MSG_GRUPPO_SANGUIGNO = "Inserisca il proprio gruppo sanguigno";
-    
+	
     //costruttore classe Paziente
     public Paziente (String nome, String cognome, String indirizzo, String telefono, String email, String dataNascita, String luogoNascita, String genere, String codiceFiscale, String codiceSanitario, String gruppoSanguigno){
 		
@@ -45,33 +34,11 @@ public class Paziente {
     }
     
     
-    private static Paziente creaPaziente() throws Exception
-	{
-		System.out.println(MSG_INTRO_INSERIMENTO);
-		String nome = InputDati.leggiStringa(MSG_NOME);
-		String cognome = InputDati.leggiStringa(MSG_COGNOME);
-		String indirizzo = InputDati.leggiStringa(MSG_INDIRIZZO);
-		String telefono = InputDati.leggiStringa (MSG_TELEFONO);
-		String email = InputDati.leggiStringa (MSG_EMAIL);
-		String dataNascita = InputDati.leggiStringa (MSG_DATA_NASCITA);
-		String luogoNascita = InputDati.leggiStringa (MSG_LUOGO_NASCITA);
-		String genere = InputDati.leggiStringa (MSG_GENERE);
-		String codiceFiscale = InputDati.leggiStringa (MSG_CODICE_FISCALE);
-		//codice sanitario
-		
-		//controllo validità dati
-		String gruppoSanguigno = InputDati.leggiStringa (MSG_GRUPPO_SANGUIGNO);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		return new Paziente (String nome, String cognome, String indirizzo, String telefono, String email, String dataNascita, String luogoNascita, String genere, String codiceFiscale, String codiceSanitario, String gruppoSanguigno);
-	}
+    
+    
+    
+   
+    	
     
     
     //necessario importare la classe InputDati, da mettere in MyLib??? è presa 
