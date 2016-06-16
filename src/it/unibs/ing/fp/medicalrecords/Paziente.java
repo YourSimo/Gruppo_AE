99477e61bc99d99c11ee87a99ff7370694a1e23d
Fedeclimber96 <federico.juve59@gmail.com>
2016-06-16 09:27:29 +0200

    Creati attributi classe Paziente

--- src/it/unibs/ing/fp/medicalrecords/Paziente.java
+++ src/it/unibs/ing/fp/medicalrecords/Paziente.java
@@ -1,5 +1,17 @@
 package it.unibs.ing.fp.medicalrecords;
 
 public class Paziente {
+	
+	String nome;
+    String cognome;
+    String indirizzo;
+    String telefono;
+    String email;
+    String dataNascita;
+    String luogoNascita;
+    String genere;
+    String codiceFiscale;
+    String codiceSanitario;
+    String gruppoSanguigno;
 
 }
