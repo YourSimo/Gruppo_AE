c1beb3f80b4b1ce50d2122eb920e9216d9392c66
Simone Cavicchioli <s.cavicchioli@studenti.unibs.it>
2016-06-22 15:44:46 +0200

    Corretto Classe EsameMisurabile

--- src/it/unibs/ing/fp/medicalrecords/EsameMisurabile.java
+++ src/it/unibs/ing/fp/medicalrecords/EsameMisurabile.java
@@ -8,26 +8,14 @@ public class EsameMisurabile extends Esame implements Serializable {
 	public final static int MAX_GLICEMIA = 99;
 	private final static int MIN_GLUCOSIO = 60;
 	private final static int MAX_GLUCOSIO = 109;
-	private final static int MIN_COLESTEROLO=0;
-	private final static int MAX_COLESTEROLO=200;
+	private final static int MIN_COLESTEROLO = 0;
+	private final static int MAX_COLESTEROLO=  200;
+	
+	//	MANCO IL COSTRUTTORE
 	
 	//verifica inserimento valore valido
-	
-	public static boolean valoreValido (int valore, int min,int max  ){
-		if (valore<min && valore>max) 
-			return false;
-		 
-		
-		return true;
-		 
-		
-		
-		
+	public static boolean valoreValido (int valore, int min, int max){
+		if (valore < min && valore > max) return false;
+		return true;	
 	}
-	
-	
-	
-	
-	
-	
 }
