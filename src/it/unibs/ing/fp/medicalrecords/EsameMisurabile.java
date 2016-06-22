f21077ce017a04ce4644afc03d445e0b9acd85e2
Matteo <Matteo@ASUS.homenet.telecomitalia.it>
2016-06-22 16:30:44 +0200

    ultime modificge

--- src/it/unibs/ing/fp/medicalrecords/EsameMisurabile.java
+++ src/it/unibs/ing/fp/medicalrecords/EsameMisurabile.java
@@ -18,10 +18,10 @@ public class EsameMisurabile extends Esame implements Serializable {
 	
 	public final static int MIN_GLICEMIA = 60;
 	public final static int MAX_GLICEMIA = 99;
-	private final static int MIN_GLUCOSIO = 60;
-	private final static int MAX_GLUCOSIO = 109;
-	private final static int MIN_COLESTEROLO = 0;
-	private final static int MAX_COLESTEROLO=  200;
+	public final static int MIN_GLUCOSIO = 60;
+	public final static int MAX_GLUCOSIO = 109;
+	public final static int MIN_COLESTEROLO = 0;
+	public final static int MAX_COLESTEROLO=  200;
 	
 	private int valore;
 	
