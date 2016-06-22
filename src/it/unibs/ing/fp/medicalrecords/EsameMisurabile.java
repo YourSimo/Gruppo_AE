c0d7372a396e176ffa2c54d0d95c8bca14578df9
Matteo <Matteo@ASUS.homenet.telecomitalia.it>
2016-06-22 12:09:53 +0200

    modifica classe esame misurabile, in utility aggiunta la parte relativa al makeexamlist

--- src/it/unibs/ing/fp/medicalrecords/EsameMisurabile.java
+++ src/it/unibs/ing/fp/medicalrecords/EsameMisurabile.java
@@ -3,5 +3,29 @@ package it.unibs.ing.fp.medicalrecords;
 import java.io.Serializable;
 
 public class EsameMisurabile extends Esame implements Serializable {
-
+	
+	public final static int MIN_GLICEMIA = 60;
+	public final static int MAX_GLICEMIA = 99;
+	private final static int MIN_GLUCOSIO = 60;
+	private final static int MAX_GLUCOSIO = 109;
+	private final static int MIN_COLESTEROLO=0;
+	private final static int MAX_COLESTEROLO=200;
+	
+	public static boolean valoreValido (int valore, int min,int max  ){
+		if (valore<min && valore>max) 
+			return false;
+		 
+		
+		return true;
+		 
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/it/unibs/ing/fp/medicalrecords/Utility.java
+++ src/it/unibs/ing/fp/medicalrecords/Utility.java
@@ -22,7 +22,15 @@ public class Utility {
 	private final static String MSG_GENERE =  "Inserisca il proprio genere:";
 	private final static String MSG_CODICE_FISCALE = "Inserisca il proprio codice fiscale";
 	private final static String MSG_GRUPPO_SANGUIGNO = "Inserisca il proprio gruppo sanguigno";
-    
+	
+	//messaggi per esame
+	
+	private final static String MSG_ESAME = "Inserisca l'esame che vuole aggiungere:";		
+	private final static String MSG_DATA = "Inserisca la data in cui è stato svolto:";
+	private final static String MSG_LUOGO = "Inserisca il luogo in cui è stato svolto:";
+	private final static String MSG_ORA = "Inserisca l'orario in cui è stato svolto:";
+	private final static String MSG_ESITO = "Inserisca l'esito dell'esame:";
+	 
 	private final static String MSG_ERRORE_INSERIMENTO = "Errore nell'inserimento dati. Dato non valido. Ritenti.";
 
 	private final static String REGEX_ALFABETO = "^[a-zA-Z]+$";
@@ -32,6 +40,8 @@ public class Utility {
 	private final static String REGEX_DATA_NASCITA = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)[0-9]{2}";
 	
 	private final static String REGEX_CODICE_FISCALE = "[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]";
+	private final static String REGEX_DATA = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)[0-9]{2}";
+	private final static String REGEX_ORA = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]){2}";
 	
 	public static CartellaSanitaria makeMedicalRecords() {
 		return new CartellaSanitaria(makePatient(), makeExamList());
@@ -115,8 +125,50 @@ public class Utility {
 	
 	private static ListaEsami makeExamList() {
 		
+		//verifica esame
+		String esame = InputData.readString(MSG_ESAME);
 		
+		while (validità Esame(esame) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    esame = null;
+			esame = InputData.readString(MSG_ESAME);	
+		};
+		//verifica data
 		
+		String data = InputData.readString(MSG_DATA);
+		
+		while (validità Data(data) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    data = null;
+			data = InputData.readString(MSG_DATA);	
+		};
+		//verifica luogo
+		
+		String luogo = InputData.readString(MSG_LUOGO);
+		
+		while (validità Luogo(luogo) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    luogo = null;
+			luogo = InputData.readString(MSG_LUOGO);	
+		};
+		//verifica ora
+		
+		String ora = InputData.readString (MSG_ORA);
+
+		while (validità Ora(ora) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    ora = null;
+			ora = InputData.readString(MSG_ORA);	
+		};
+		//verifica esito
+		
+		String esito = InputData.readString (MSG_ESITO);
+		
+		while (validità Esito(esito) == false){
+			System.out.println(MSG_ERRORE_INSERIMENTO);
+		    esito = null;
+			esito = InputData.readString(MSG_ESITO);	
+		};
 		
 		
 		
