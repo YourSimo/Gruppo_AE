40c6111c34fc8baa87eb22135f2ecc01ed6e3d40
Simone Cavicchioli <s.cavicchioli@studenti.unibs.it>
2016-06-22 15:42:14 +0200

    Aggiornato descrizione Paziente

--- src/it/unibs/ing/fp/medicalrecords/Paziente.java
+++ src/it/unibs/ing/fp/medicalrecords/Paziente.java
@@ -14,15 +14,15 @@ public class Paziente implements Serializable {
 	private static final String TITOLO = "SCHEDA PAZIENTE";
 	private static final String DESCRIZIONE = "Nome: %s%n"
 											+ "Cognome: %s%n"
-											+ "Indirizzo:%s%n"
-											+ "Telefono:%s%n"
-											+ "Email:%s%n"
-											+ "Data di nascita:%s%n"
-											+ "Luogo di nascita:%s%n"
-											+ "Genere:%s%n"
-											+ "Codice Fiscale:%s%n"
-											+ "Codice Sanitario:%s%n"
-											+ "Gruppo Sanguigno:%s%n";
+											+ "Indirizzo: %s%n"
+											+ "Telefono: %s%n"
+											+ "Email: %s%n"
+											+ "Data di nascita: %s%n"
+											+ "Luogo di nascita: %s%n"
+											+ "Genere: %s%n"
+											+ "Codice Fiscale: %s%n"
+											+ "Codice Sanitario: %s%n"
+											+ "Gruppo Sanguigno: %s%n";
 
 	
 	private String nome;
